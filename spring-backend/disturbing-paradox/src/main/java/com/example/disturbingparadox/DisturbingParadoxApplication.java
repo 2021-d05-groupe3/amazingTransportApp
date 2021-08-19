c9acc100389cacb8a5dadcc211db9bab44cf59cc
x06c <jonathan.miss@outlook.com>
2021-08-19 13:49:43 +0200

    spring commit

---
+++ spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/DisturbingParadoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.disturbingparadox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisturbingParadoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisturbingParadoxApplication.class, args);
+	}
+
+}
