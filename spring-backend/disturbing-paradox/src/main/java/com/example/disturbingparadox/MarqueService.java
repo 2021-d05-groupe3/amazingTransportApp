8c4ad2b4683582a77341828cb06d8d9d0460b85f
Anthony Stephan <66592261+anthony1334@users.noreply.github.com>
2021-08-19 16:14:17 +0200

    test upperCase

--- spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/MarqueService.java
+++ spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/MarqueService.java
@@ -13,7 +13,9 @@ public class MarqueService {
 
     public List<Marque> getMarque(String nom) {
 		System.out.println(" hello coco!");
+        System.out.println(" hello jo!");
 		return  marqueRepository.findByNom(nom);
+        
 		
 	}
 	
