051691eb8dbd0a29fa215222aa13f9752aec2199
Anthony Stephan <66592261+anthony1334@users.noreply.github.com>
2021-08-19 15:52:05 +0200

    tryng connect to database

---
+++ spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/MarqueService.java
@@ -0,0 +1,21 @@
+package com.example.disturbingparadox;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MarqueService {
+
+    @Autowired
+    MarqueRepository marqueRepository;
+
+    public List<Marque> getMarque(String nom) {
+		System.out.println(" hello coco!");
+		return  marqueRepository.findByNom(nom);
+		
+	}
+	
+    
+}
--- spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/marque.java
+++ spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/marque.java
@@ -1,5 +1,44 @@
 package com.example.disturbingparadox;
 
-public class marque {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Marque {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+    Long id ;
+    @Column()
+    String nom, pays;
+
+    public Long getId() {
+        return id;
+    }
+
+   
+
+    public Marque() {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+
+    
     
 }
