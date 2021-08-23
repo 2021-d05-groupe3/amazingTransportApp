47ff8753441eb39a680a4c1ecd600d330340ada9
CMM <chrystellemm@gmail.com>
2021-08-23 15:21:17 +0200

    Ajout detail covoiturage

--- client/src/app/covoiturage/covoiturages/covoiturage-detail/covoiturage-detail.component.ts
+++ client/src/app/covoiturage/covoiturages/covoiturage-detail/covoiturage-detail.component.ts
@@ -1,4 +1,9 @@
+import { CovoiturageService } from './../../../services/covoiturage.service';
+
+
+import { Covoiturage } from './../../../covoiturage';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-covoiturage-detail',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CovoiturageDetailComponent implements OnInit {
 
-  constructor() { }
+  id!: number;
+  covoiturage!: Covoiturage;
+
+  constructor(private route : ActivatedRoute, private covoiturageService : CovoiturageService) { }
 
   ngOnInit(): void {
-  }
+    this.id = +!this.route.snapshot.paramMap.get('id');
+    //on va chercher le covoit dont l'id correspond à l'id récupéré
+    this.getCovoiturage();
 
+  }
+getCovoiturage(){
+  this.covoiturageService.getCovoiturage(this.id).subscribe((data) => this.covoiturage = data);
+}
+save(){
+  this.covoiturageService.updateCovoiturage(this.covoiturage).subscribe( () => console.log(this.covoiturage, "enregistré"))
+}
 }
