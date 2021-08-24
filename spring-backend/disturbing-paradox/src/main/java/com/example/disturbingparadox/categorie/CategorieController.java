c6425d876101bfdb5e6d8ef92b829ae9df719394
Anthony Stephan <66592261+anthony1334@users.noreply.github.com>
2021-08-24 14:10:14 +0200

    add get/post/delete categorie

--- spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/categorie/CategorieController.java
+++ spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/categorie/CategorieController.java
@@ -1,10 +1,13 @@
 package com.example.disturbingparadox.categorie;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,8 +24,22 @@ if(oCategorie.isPresent()){
 }
 return null;
 		
+	}
 
+	@GetMapping("/afficherCategorie")
+	public List<Categorie> afficherLesCategories(){
+		return categorieService.getCategories();
+	}
 
+	@PostMapping("/ajouterCategorie")
+	public void ajouterCategorie(Categorie categorie) {
+		categorieService.addCategorie(categorie);
+	}
+
+	@DeleteMapping("/suprimeCategorie/{id}")  	
+	public String supprimeCategorie(@PathVariable("id") Long id) {
+		categorieService.deleteCategorie(id);
+	     return"supression ok";
 	}
     
 }
--- spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/categorie/CategorieService.java
+++ spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/categorie/CategorieService.java
@@ -1,6 +1,7 @@
 package com.example.disturbingparadox.categorie;
 
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,23 @@ public class CategorieService {
     public Optional<Categorie> getCategorie(Long id) {
 
 		return  categorieRepository.findById(id);
-        
-		
+        	
+	}
+
+  
+  public List<Categorie> getCategories(){
+		return categorieRepository.findAll();
+	}
+
+  public void addCategorie(Categorie nouvelleCategorie) {
+		categorieRepository.save(nouvelleCategorie);
+	}
+
+  public void deleteCategorie(Long id) {
+		for(Categorie c: categorieRepository.findAll() ) {
+			if(id.equals(c.getId()))
+      categorieRepository.delete(c);
+		}
 	}
     
 }
