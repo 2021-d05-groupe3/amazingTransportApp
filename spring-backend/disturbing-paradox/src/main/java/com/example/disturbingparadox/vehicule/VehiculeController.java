cc484a71d7d0f076c6b4c50263d9252b65a9e9d8
Anthony Stephan <66592261+anthony1334@users.noreply.github.com>
2021-08-24 11:24:54 +0200

    update vehicule

--- spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/vehicule/VehiculeController.java
+++ spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/vehicule/VehiculeController.java
@@ -1,6 +1,6 @@
 package com.example.disturbingparadox.vehicule;
+import java.util.Optional;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +12,13 @@ public class VehiculeController {
     @Autowired
 	VehiculeService vehiculeService;
 
-    @GetMapping("/vehicule/{nom}")
-	public List<Vehicule>  getVehicule(@PathVariable String nom) {
-		List<Vehicule> vehicules = vehiculeService.getVehicule(nom);
-
-		return vehicules;
+    @GetMapping("/vehicule/{id}")
+	public Vehicule  getVehicule(@PathVariable Long id) {
+		Optional<Vehicule> oVehicule = vehiculeService.getVehicule(id);
+if(oVehicule.isPresent()){
+	return oVehicule.get();
+}
+		return null;
 
 
 	}
