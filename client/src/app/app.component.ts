08ea48baef325efb95bd50059e67eeb4997949a9
EnzoCALL <enzo.12.juin@gmail.com>
2021-08-27 09:41:24 +0200

    Login - en cours

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { FormControl, FormGroup } from '@angular/forms'
+import { ReactiveFormsModule } from '@angular/forms';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
+
+  template: `<button (click)="compare()">Login</button> {{clickMessage}}`
 })
-export class AppComponent {
-  title = 'client';
+
+export class AppComponent implements OnInit {
+  title = 'Angular Example';
+  products: any = [];
+
+  email = new FormControl();
+  password = new FormControl();
+
+  constructor(private httpClient: HttpClient){}
+  ngOnInit(){
+    this.httpClient.get("assets/users.json").subscribe(users =>{console.log(users); //CONSOLE LOG TO REMOVE, FOR TEST PURPOSE ONLY
+      this.products = users;
+    })
+  }
+  onClick(){
+    this.ngOnInit;
+    console.log(this.email);
+  }
+
+
 }
+
