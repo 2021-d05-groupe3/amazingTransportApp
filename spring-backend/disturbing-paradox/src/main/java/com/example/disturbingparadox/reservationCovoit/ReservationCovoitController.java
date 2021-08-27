61945e3a6039c425cf05359935eb8c5e9e6ab9d6
Anthony Stephan <66592261+anthony1334@users.noreply.github.com>
2021-08-27 15:55:57 +0200

    update reservationCovoit and add postMapping

--- spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/reservationCovoit/ReservationCovoitController.java
+++ spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/reservationCovoit/ReservationCovoitController.java
@@ -1,10 +1,19 @@
 package com.example.disturbingparadox.reservationCovoit;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class ReservationCovoitController {
     
     @Autowired
     ReservationCovoitService reservationCovoitService;
+
+    @PostMapping("/reservationCovoit")
+	public ReservationCovoit ajouterReservationCovoit(@RequestBody ReservationCovoitDto reservationCovoit) {
+		
+		return reservationCovoitService.addModel(reservationCovoit);
+	}
 
 }
--- spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/reservationCovoit/ReservationCovoitService.java
+++ spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/reservationCovoit/ReservationCovoitService.java
@@ -1,10 +1,19 @@
 package com.example.disturbingparadox.reservationCovoit;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
+@Service
 public class ReservationCovoitService {
 
     @Autowired
     ReservationCovoitRepository reservationCovoitRepository;
+
+    public ReservationCovoit addModel(ReservationCovoitDto nouvelleReservationCovoit) {
+        ReservationCovoit reservationCovoit = new ReservationCovoit();
+        reservationCovoit.setDateDeparture(nouvelleReservationCovoit.getDateDeparture());
+        reservationCovoit.setDateArrival(nouvelleReservationCovoit.getDateArrival());
+        
+          return reservationCovoitRepository.save(reservationCovoit);
+      }
     
 }
