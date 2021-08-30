e5699fe75dfe9837ccac92dcf609d5849b95dbbb
EnzoCALL <enzo.12.juin@gmail.com>
2021-08-30 16:36:00 +0200

    -logging in works -catches user's class (user/driver/admin) -commentaries added

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { FormControl, FormGroup } from '@angular/forms'
-import { ReactiveFormsModule } from '@angular/forms';
-import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-login',
@@ -16,23 +13,49 @@ import { ThrowStmt } from '@angular/compiler';
 export class LoginComponent implements OnInit {
   title = 'Angular Example';
   products: any = [];
-
-  email = new FormControl();
-  password = new FormControl();
+  i = 0;  //Increment for mail
+  j = 0;  //Increment for password
+  iMail = -1; //Stores which user matches the mail
+  jPass = -2; //Stores which user matches the password
+  isValid:boolean = false;  //True = Move to next page | Only true if iMail == jPass
+  ;
 
   constructor(private httpClient: HttpClient){}
   ngOnInit(){
-    this.httpClient.get("assets/users.json").subscribe(users =>{console.log(users); //CONSOLE LOG TO REMOVE, FOR TEST PURPOSE ONLY
-      this.products = users;
+    this.httpClient.get("assets/users.json").subscribe(users =>{console.log("Database accessed"); //Accesses the json database | console.log(users); to display the json DB
+      this.products = users; //products stores the json database
     })
   }
-  onClick(){
+  onClick(){ //When the button is clicked
     this.ngOnInit;
-    var val = (document.getElementById('lemail')as HTMLInputElement).value;
-    var val2 = (document.getElementById('lepass')as HTMLInputElement).value;
+ 
+    var val = (document.getElementById('lemail')as HTMLInputElement).value; //Receives input from the HTML email field
 
-    console.log(val);
-    console.log(val2);
+    for(this.i=0; this.i< this.products.length; this.i++) //For each user in database
+      if (this.products[this.i].email == val) //If input email matches a user's email in databse
+      {
+        this.iMail = this.i;
+        console.log("True"); //for test purpose
+      }
+      else{
+      }
+
+    var val2 = (document.getElementById('lepass')as HTMLInputElement).value; //Receives input from the HTML password field
+
+    for(this.j=0; this.j< this.products.length; this.j++) //For each user in database
+      if (this.products[this.j].password == val2) //If input password matches a user's password in databse
+      {
+        this.jPass = this.j;
+        console.log("True");  //for test purpose
+      }
+      else{
+      }
+
+      //console.log(this.iMail); console.log(this.jPass);  //Check if iMail & jPass are affected
+
+      if (this.iMail == this.jPass){this.isValid = true; //If the email & the password both match the same user
+      console.log("Succés. Vous êtes un utilisateur classé: " + this.products[this.jPass].class);}  //Success + displays user's class | this.products[this.jPass].class = to catch user's class
+      else{console.log("Le mot de passe de correspond pas à l'email.");} //If the email & the password don't match the same user
   }
 }
 
