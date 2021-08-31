bc2d09b69579e441ba2488528db4495b3c9933eb
x06c <jonathan.miss@outlook.com>
2021-08-31 11:42:33 +0200

    add GET method

--- spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/reservationCovoit/ReservationCovoitController.java
+++ spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/reservationCovoit/ReservationCovoitController.java
@@ -1,10 +1,18 @@
 package com.example.disturbingparadox.reservationCovoit;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class ReservationCovoitController {
     
     @Autowired
     ReservationCovoitService reservationCovoitService;
+
+    @GetMapping("/reservation-covoiturage")
+    public List<ReservationCovoit> getReservations() {
+        return reservationCovoitService.getReservationCovoit();
+    }
 
 }
--- spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/reservationCovoit/ReservationCovoitService.java
+++ spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/reservationCovoit/ReservationCovoitService.java
@@ -1,10 +1,18 @@
 package com.example.disturbingparadox.reservationCovoit;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class ReservationCovoitService {
 
     @Autowired
     ReservationCovoitRepository reservationCovoitRepository;
+
+    public List<ReservationCovoit> getReservationCovoit() {
+
+        return reservationCovoitRepository.findAll();
+
+    }
     
 }
