b004e98b2252137bcc9d68ed2d3fc19c885491d5
Anthony Stephan <66592261+anthony1334@users.noreply.github.com>
2021-09-01 15:42:37 +0200

    update route reservationVehiculeentreprise and add getMapping

--- spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/reservationVehiculeEntreprise/ReservationVehiculeEntrepriseController.java
+++ spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/reservationVehiculeEntreprise/ReservationVehiculeEntrepriseController.java
@@ -1,6 +1,9 @@
 package com.example.disturbingparadox.reservationVehiculeEntreprise;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReservationVehiculeEntrepriseController {
     @Autowired
     ReservationVehiculeEntrepriseService reservationVehiculeEntrepriseService;
+
+    @GetMapping("/reservationVehiculeEntreprises")
+	public List<ReservationVehiculeEntreprise> afficherLesAnnonces(){
+		return reservationVehiculeEntrepriseService.getReservationVehiculeEntreprises();
+	}
 
     @PostMapping("/reservationVehiculeEntreprise")
 	public ReservationVehiculeEntreprise ajouterReservationVehiculeEntreprise(@RequestBody ReservationVehiculeEntrepriseDto ReservationVehiculeEntreprise) {
--- spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/reservationVehiculeEntreprise/ReservationVehiculeEntrepriseService.java
+++ spring-backend/disturbing-paradox/src/main/java/com/example/disturbingparadox/reservationVehiculeEntreprise/ReservationVehiculeEntrepriseService.java
@@ -1,5 +1,6 @@
 package com.example.disturbingparadox.reservationVehiculeEntreprise;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.example.disturbingparadox.collaborateur.Collaborateur;
@@ -17,6 +18,10 @@ public class ReservationVehiculeEntrepriseService {
     CollaborateurRepository collaborateurRepository;
     @Autowired
     VehiculeSocieteRepository vehiculeSocieteRepository;
+
+    public List<ReservationVehiculeEntreprise> getReservationVehiculeEntreprises(){
+		return reservationVehiculeEntrepriseRepository.findAll();
+	}
 
     public ReservationVehiculeEntreprise addReservationVehiculeEntreprise(ReservationVehiculeEntrepriseDto nouvelleReservationVehiculeEntreprise) {
         ReservationVehiculeEntreprise reservationVehiculeEntreprise = new ReservationVehiculeEntreprise();
